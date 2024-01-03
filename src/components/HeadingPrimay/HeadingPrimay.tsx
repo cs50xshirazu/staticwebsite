b066f025d324d403f3c5148c2d579fa35fa202c4
pouria-hjt <poriw.hjt@gmail.com>
2024-01-03 17:53:38 +0330

    change gap between heading items

--- src/components/HeadingPrimay/HeadingPrimay.tsx
+++ src/components/HeadingPrimay/HeadingPrimay.tsx
@@ -10,7 +10,7 @@ export const HeadingPrimay = ({ title, children, className }: Props) => {
   return (
     <div className="w-full min-h-10 flex justify-between items-center gap-4">
       <img src="/images/line.svg" className="w-2/5 h-full rotate-180" alt="" />
-      <div className="w-1/5 flex justify-evenly">
+      <div className="w-1/5 flex justify-center gap-2">
         {children && children}
         {title && (
           <HeadingPrimayText
