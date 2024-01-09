c59c2b53fdc862f9aa9d29ae64bc2a3917d6fdb3
pouria-hjt <poriw.hjt@gmail.com>
2024-01-09 14:02:40 +0330

    add responsive for about cs50x

--- src/features/AboutCS50x/AboutCS50x.feature.tsx
+++ src/features/AboutCS50x/AboutCS50x.feature.tsx
@@ -12,8 +12,11 @@ type Props = {};
 export const AboutCS50x = (props: Props) => {
   return (
     <section className="w-full flex flex-col gap-6 mt-14 p-page">
-      <HeadingPrimay className="font-GEO703 text-4.5xl" title="CS50x SHIRAZU" />
-      <p className="text-justify p-5 text-quinary text-2xl font-semibold">
+      <HeadingPrimay
+        className="font-GEO703 text-4.5xl lg:text-3xl"
+        title="CS50x SHIRAZU"
+      />
+      <p className="text-justify p-5 text-quinary text-2xl font-semibold lg:text-lg">
         CS50 دوره آموزش مبانی علوم کامپیوتر و آشنایی با منطق و الفبای برنامه
         نویسی است که نخستین بار در دانشگاه های هاروارد و ییل ارائه شد. دوره ی سی
         اس فیفتی یک دوره ی Open-License است که به تمام دنیا این اجازه را میدهد
@@ -30,7 +33,7 @@ export const AboutCS50x = (props: Props) => {
         className="font-IranYekan text-3xl"
         title="ویژگی های دوره ی ما"
       />
-      <p className="text-justify p-5 text-quinary text-2xl font-semibold">
+      <p className="text-justify p-5 text-quinary text-2xl font-semibold lg:text-lg">
         در پایان دوره های ما میتوانید گواهی معتبر پایان دوره برای دوره ی ثبت
         نامی خود را از دانشگاه هاروارد و گواهی حضور در دوره را از دانشگاه شیراز
         دریافت کنید. گواهی پایان دوره تحت دامنه دانشگاه هاروارد و گواهی حضور تحت
--- src/features/Header/Header.feature.tsx
+++ src/features/Header/Header.feature.tsx
@@ -7,7 +7,7 @@ export const Header = (props: Props) => {
     <header className="w-full h-[calc(100vh-8rem)] px-32 bg-headerImage bg-cover bg-center header-bg bg-blend-multiply flex justify-start relative">
       <div className="absolute bottom-5 right-24 text-quaternary font-GEO703 flex flex-col items-end font-bold">
         <span className="text-2.5xl w-fit">Welcome to</span>
-        <h1 className="text-7xl">CS50x SHIRAZU</h1>
+        <h1 className="text-7xl xl:text-5xl md:text-2xl">CS50x SHIRAZU</h1>
       </div>
     </header>
   );
