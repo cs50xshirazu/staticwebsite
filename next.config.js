fa30a63bb27625156b2d9de19e2a2343122130e6
puria-hjt <poriw.hjt@gmail.com>
2024-06-29 14:40:47 +0200

    ref(nextconf): output export and not upt image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
-}
+  output: "export", // <=== enables static exports
+  // reactStrictMode: true,
+  images: { unoptimized: true },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
