68411443e35091c0b89ce6b7414fb2a744647e06
puria-hjt <poriw.hjt@gmail.com>
2024-06-29 15:05:09 +0200

    fix photos

--- src/features/NavBar/NavBar.feature.tsx
+++ src/features/NavBar/NavBar.feature.tsx
@@ -4,14 +4,22 @@ import React, { useState } from "react";
 import { HamMenu } from "..";
 type Props = {};
 
+import Logo from "../../../public/images/logo.png";
+
 export const NavBar = (props: Props) => {
   return (
     <>
       <nav className="w-full h-32 px-28 2xl:px-10 md:!px-2 py-2.5 flex justify-center items-center flex-col md:hidden">
         <div className="w-full h-fit flex justify-between items-center">
-          <ButtonPrimary title="ورود به پنل کاربری" onClick={() =>location.href="https://panel.cs50xshirazu.ir"} />
-          <img src="/images/logo.png" className="h-20 w-14" alt="" />
-          <ButtonPrimary title="ثبت نام آنلاین" onClick={() =>window.open('https://cs50xshirazu.ir','_blank')} />
+          <ButtonPrimary
+            title="ورود به پنل کاربری"
+            onClick={() => (location.href = "https://panel.cs50xshirazu.ir")}
+          />
+          <Image src={Logo} className="h-20 w-14" alt="" />
+          <ButtonPrimary
+            title="ثبت نام آنلاین"
+            onClick={() => window.open("https://cs50xshirazu.ir", "_blank")}
+          />
         </div>
         <HeadingPrimay>
           <HeadingPrimayText
@@ -19,11 +27,7 @@ export const NavBar = (props: Props) => {
             link="#whatIsCs50"
             title="Cs50 چیست؟"
           />
-          <HeadingPrimayText
-            isHeading={false}
-            link="#team"
-            title="تیم ما"
-          />
+          <HeadingPrimayText isHeading={false} link="#team" title="تیم ما" />
           <HeadingPrimayText
             isHeading={false}
             link="#faq"
