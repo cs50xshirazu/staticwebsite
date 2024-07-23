e62d25945f32a53dbfe7710d21dd89770004e5d1
Mansour <84951627+marzban-dev@users.noreply.github.com>
2024-07-23 14:11:28 +0330

    Create employees section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,29 +5,17 @@ import Faq from "@/app/components/Faq";
 import Footer from "@/app/components/Footer";
 import Courses from "@/app/components/Courses";
 import Sponsors from "@/app/components/Sponsors";
-import BackgroundPattern from "@/app/components/BackgroundPattern";
-import Navbar from "@/app/components/Navbar";
-import SideDrawer from "@/app/components/SideDrawer";
-import Header from "@/app/components/Header";
-import Employees from "@/app/components/Employees";
 
 export default function Home() {
     return (
-        <>
-            <BackgroundPattern />
-            <Navbar />
-            <SideDrawer />
-            <Header />
-            <main className="">
-                <About />
-                <Features />
-                <Courses />
-                <Sponsors />
-                <Preferences />
-                <Employees />
-                <Faq />
-                <Footer />
-            </main>
-        </>
+        <main className="">
+            <About />
+            <Features />
+            <Courses />
+            <Sponsors />
+            <Preferences />
+            <Faq />
+            <Footer />
+        </main>
     );
 }
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,31 +1,20 @@
-export {};
+export {}
 
 declare global {
     type Course = {
-        image: string;
-        title: string;
-        link: string;
+        image : string;
+        title : string;
+        link : string;
     }
 
     type Sponsor = {
-        title: string;
-        image: string;
-        link: string;
+        title : string;
+        image : string;
+        link : string;
     }
 
     type QuestionAnswer = {
-        title: string;
-        description: description;
-    }
-
-    type Employee = {
-        name: string;
-        job: string;
-        image : string;
-        socialMedias?: {
-            title: string,
-            link: string;
-            icon: string;
-        }[]
+        title : string;
+        description : description;
     }
 }
